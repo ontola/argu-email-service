0c986b8a467107df3c8096cf43f4ef46fc2e1080
Arthur Dingemans <arthur@argu.co>
2016-12-02 15:29:58 +0100

    [IMP] Move Bugsnag api_key to env

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 Bugsnag.configure do |config|
-  config.api_key = 'b65723480764039f0c48b01009935be9'
+  config.api_key = ENV['BUGSNAG_KEY']
   config.notify_release_stages = %w(production staging)
   config.app_version = "v#{::VERSION}/#{::BUILD}"
 end
