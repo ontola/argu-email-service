1781c3450d18eb8112274a6833055e3341abd290
Arthur Dingemans <arthur@argu.co>
2017-03-20 14:51:24 +0100

    [ES-4] Dont include mailgun_id in serialiser

--- app/serializers/email_serializer.rb
+++ app/serializers/email_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 class EmailSerializer < ActiveModel::Serializer
-  attributes :id, :sent_to, :sent_at, :mailgun_id
+  attributes :id, :sent_to, :sent_at
   has_many :email_events
 end
