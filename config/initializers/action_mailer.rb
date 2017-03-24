4cf46c0991788f4c9bedd5e2a85b53214fdb9a9f
Arthur Dingemans <arthur@argu.co>
2017-03-24 15:54:22 +0100

    [FIX] Mail address config

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,19 +1,17 @@
 # frozen_string_literal: true
-Rails.application.configure do
-  if Rails.env.development? || Rails.env.staging?
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address: '127.0.0.1',
-      port: ENV['MAIL_PORT'].presence || 1025
-    }
+if Rails.env.development? || Rails.env.staging?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: ENV['MAIL_ADDRESS'].presence || '127.0.0.1',
+    port: ENV['MAIL_PORT'].presence || 1025
+  }
 
-    config.action_mailer.perform_caching = false
-  elsif Rails.env.production?
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :mailgun
-    config.action_mailer.mailgun_settings = {
-      api_key: Rails.application.secrets.mailgun_api_token,
-      domain: Rails.application.secrets.mailgun_domain
-    }
-  end
+  ActionMailer::Base.perform_caching = false
+elsif Rails.env.production?
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.delivery_method = :mailgun
+  ActionMailer::Base.mailgun_settings = {
+    api_key: Rails.application.secrets.mailgun_api_token,
+    domain: Rails.application.secrets.mailgun_domain
+  }
 end
