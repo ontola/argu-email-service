606a6aa184973464e164a1604c1277323ad47cd3
Arthur Dingemans <arthur@argu.co>
2017-07-03 17:04:36 +0200

    [ES-13] Use update!

--- app/models/email.rb
+++ app/models/email.rb
@@ -5,7 +5,7 @@ class Email < ApplicationRecord
 
   def deliver_now
     r = event.mailer.send(template, self).deliver_now
-    update(sent_at: DateTime.current, sent_to: r.to.first, mailgun_id: r.try(:message_id))
+    update!(sent_at: DateTime.current, sent_to: r.to.first, mailgun_id: r.try(:message_id))
   end
 
   def recipient
--- app/models/event.rb
+++ app/models/event.rb
@@ -11,11 +11,11 @@ class Event < ApplicationRecord
 
   def enqueue_job
     if mailer.nil?
-      update(processed_at: DateTime.current)
+      update!(processed_at: DateTime.current)
       return
     end
     return if job_is_active?
-    update(job_id: ProcessEventJob.perform_async(id))
+    update!(job_id: ProcessEventJob.perform_async(id))
   end
 
   def desired_emails
