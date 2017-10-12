6bc7b98a6356d7cada5e63aa206711999b170511
Arthur Dingemans <arthur@argu.co>
2017-10-12 13:03:40 +0200

    [ES-29] Add confirm_votes and set_password mails

--- app/mailers/confirmations_mailer.rb
+++ app/mailers/confirmations_mailer.rb
@@ -13,6 +13,14 @@ class ConfirmationsMailer < ApplicationMailer
     roadie_mail(to: opts[:email], subject: t('confirmations.requested_confirmation.subject'))
   end
 
+  def set_password
+    roadie_mail(to: recipient.email, subject: t('confirmations.set_password.subject'))
+  end
+
+  def confirm_votes
+    roadie_mail(to: recipient.email, subject: t('confirmations.confirm_votes.subject', count: opts[:motions].count))
+  end
+
   def self.show_footer?
     false
   end
--- spec/requests/create_emails_spec.rb
+++ spec/requests/create_emails_spec.rb
@@ -106,4 +106,53 @@ describe 'Create emails', type: :request do
     assert_equal ActionMailer::Base.deliveries.first.subject, 'Bevestig jouw e-mailadres'
     assert_match 'confirmationToken', ActionMailer::Base.deliveries.first.body.encoded
   end
+
+  it 'posts confirm_votes email' do
+    Sidekiq::Worker.drain_all
+    assert_difference('ActionMailer::Base.deliveries.size', 1) do
+      post '/spi/emails',
+           params: {
+             email: {
+               mailer: 'ConfirmationsMailer',
+               template: 'confirm_votes',
+               recipient: {email: 'test@example.com'},
+               options: {
+                 confirmationToken: 'confirmationToken', motions: [
+                   {display_name: 'Motion 1', url: '', option: 'pro'},
+                   {display_name: 'Motion 2', url: '', option: 'con'}
+                 ]
+               }
+             }
+           }
+      expect(response.code).to eq('201')
+    end
+    assert(Email.last.sent_at)
+    assert_equal Email.last.sent_to, 'test@example.com'
+    assert_equal Email.last.mailer, 'ConfirmationsMailer'
+    assert_equal ActionMailer::Base.deliveries.first.subject, 'Bevestig jouw stemmen'
+    assert_match 'V=C3=B3=C3=B3r', ActionMailer::Base.deliveries.first.body.encoded
+    assert_match 'Motion 1', ActionMailer::Base.deliveries.first.body.encoded
+    assert_match 'Tegen', ActionMailer::Base.deliveries.first.body.encoded
+  end
+
+  it 'posts set_password email' do
+    Sidekiq::Worker.drain_all
+    assert_difference('ActionMailer::Base.deliveries.size', 1) do
+      post '/spi/emails',
+           params: {
+             email: {
+               mailer: 'ConfirmationsMailer',
+               template: 'set_password',
+               recipient: {email: 'test@example.com'},
+               options: {passwordToken: 'passwordToken'}
+             }
+           }
+      expect(response.code).to eq('201')
+    end
+    assert(Email.last.sent_at)
+    assert_equal Email.last.sent_to, 'test@example.com'
+    assert_equal Email.last.mailer, 'ConfirmationsMailer'
+    assert_equal ActionMailer::Base.deliveries.first.subject, 'Stel een wachtwoord in'
+    assert_match 'passwordToken', ActionMailer::Base.deliveries.first.body.encoded
+  end
 end
