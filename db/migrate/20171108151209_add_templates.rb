fdd4387a494897e1ce140eb6f4bc01588a44e230
Arthur Dingemans <arthur@argu.co>
2017-11-08 16:45:14 +0100

    [ES-35] Add confirmation reminder email

--- db/migrate/20171108151209_add_templates.rb
+++ db/migrate/20171108151209_add_templates.rb
@@ -2,5 +2,6 @@ class AddTemplates < ActiveRecord::Migration[5.0]
   def change
     Template.create!(name: 'reset_password_instructions', show_footer: false)
     Template.create!(name: 'unlock_instructions', show_footer: false)
+    Template.create!(name: 'confirmation_reminder', show_footer: false)
   end
 end
