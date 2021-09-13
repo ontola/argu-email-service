cfabde30a08745fafb3fab1a2652c62710f7db33
Arthur Dingemans <arthur@argu.co>
2021-09-13 10:06:52 +0200

    core#610 Add LOCAL_TEST_MODE option

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if Rails.env.development? || Rails.env.staging?
+if Rails.env.development? || Rails.env.staging? || ENV['LOCAL_TEST_MODE'] == 'true'
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address: ENV['MAIL_ADDRESS'].presence || '127.0.0.1',
