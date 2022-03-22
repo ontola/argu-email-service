00a8ed18999980a12c3d6a723ebb2f730a561da6
Arthur Dingemans <arthur@argu.co>
2022-03-22 11:56:48 +0100

    core#12 Fix migration version

--- db/migrate/20220322082812_add_drafts_reminder.rb
+++ db/migrate/20220322082812_add_drafts_reminder.rb
@@ -1,4 +1,4 @@
-class AddDraftsReminder < ActiveRecord::Migration[7.0]
+class AddDraftsReminder < ActiveRecord::Migration[5.2]
   def change
     Template.create!(name: 'drafts_reminder', show_footer: false)
   end
