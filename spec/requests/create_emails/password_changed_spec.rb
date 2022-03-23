9546fdf3a574a8e9a6ae42072b1e817150e5e1ca
Arthur Dingemans <arthur@argu.co>
2022-03-23 13:57:45 +0100

    core#1016 Send password_changed email through API

---
+++ spec/requests/create_emails/password_changed_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'support/seeds'
+
+describe 'Password changed', type: :request do
+  it 'posts email' do
+    as_service
+    Sidekiq::Worker.drain_all
+    assert_difference('ActionMailer::Base.deliveries.size', 1) do
+      post '/argu/email/spi/emails',
+           params: {
+             email: {
+               template: 'password_changed',
+               recipient: {email: 'test@email.com'}
+             }
+           }, headers: service_headers
+      expect(response.code).to eq('201')
+    end
+    assert(EmailMessage.last.sent_at)
+    assert_equal EmailMessage.last.sent_to, 'test@email.com'
+    assert_equal ActionMailer::Base.deliveries.first.subject, 'Your password has been updated'
+  end
+end
